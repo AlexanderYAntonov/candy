e0322e8c6f9edc6c74b6527729e9f01f45217b30
Alexander Antonov <a.antonov76358@gmail.com>
2020-04-09 10:16:42 +0700

    добавил импорт данных из json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Album from './components/Album';
+import { Album } from './components/Album';
 
-function App() {
-  return (
-    <div className="App">
-      <Album />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    list: [],
+    isLoading: false
+  };
+
+  componentDidMount() {
+    const url = 'http://localhost:3000/assets/json/cakesList.json';
+    this.setState({ isLoading: true });
+    fetch(url).then((response) => response.json()).then((data) => {
+      console.log(data);
+      this.setState({ isLoading: false, list: data });
+    });
+  }
+
+  render() {
+    const { list } = this.state;
+    return (
+      <div className="App">
+        <Album list={list} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Album.js
+++ src/components/Album.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
+// import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import MenuIcon from '@material-ui/icons/Menu';
+// import MenuIcon from '@material-ui/icons/Menu';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
+// import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
+// import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import PropTypes from 'prop-types';
 import './Album.css';
 
 function Copyright() {
@@ -28,54 +28,42 @@ function Copyright() {
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2)
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    backgroundImage:
-      'url("http://localhost:3000/assets/images/hero-image-cut.jpg")',
-    padding: theme.spacing(8, 0, 6),
-    height: '50vh',
-    position: 'relative'
-  },
-  heroButtons: {
-    // marginTop: theme.spacing(4)
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8)
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  cardMedia: {
-    paddingTop: '56.25%' // 16:9
-  },
-  cardContent: {
-    flexGrow: 1
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6)
-  },
-  toolbar: {
-    backgroundColor: '#143109'
-  }
-}));
+class Album extends React.Component {
+  buildTemplate = () => {
+    const { list } = this.props;
+    const prefix = 'http://localhost:3000/assets/images/';
+    return list.map((card) => (
+      <Grid item key={card.id} xs={12} sm={6} md={4}>
+        <Card className={'card'}>
+          <CardMedia
+            className={'cardMedia'}
+            image={prefix + card.imgSrc}
+            title={card.title}
+          />
+          <CardContent className={'cardContent'}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {card.title}
+            </Typography>
+            <Typography>{card.shortDescription}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" className={'cardButton'}>
+              Подробнее
+            </Button>
+            <Button size="small" className={'cardButton'}>
+              Заказать
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    ));
+  };
 
-const cards = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
-
-export default function Album() {
-  const classes = useStyles();
-
-  return (
-    <React.Fragment>
-      {/* <CssBaseline /> */}
-      {/*
+  render() {
+    return (
+      <React.Fragment>
+        {/* <CssBaseline /> */}
+        {/*
       <AppBar position="relative">
         <Toolbar className={classes.toolbar}>
           <MenuIcon className={classes.icon} />
@@ -85,115 +73,45 @@ export default function Album() {
         </Toolbar>
       </AppBar>
       */}
-      <main>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <div className={'heroButtons'}>
-            <Button variant="contained" className={'mainButton'}>
-              Заказать
-            </Button>
-            {/* 
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" className={classes.mainButton}>
-                    Заказать
-                  </Button>
-                </Grid> 
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button>
-                </Grid>
-                
-              </Grid>
-              */}
-          </div>
-          {/*
-          <Container maxWidth="sm" className={'heroContainer'}>
-            
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="textPrimary"
-              gutterBottom
-            />
-            <Typography
-              variant="h5"
-              align="center"
-              color="textSecondary"
-              paragraph
-            >
-              
-            </Typography>
+        <main>
+          {/* Hero unit */}
+          <div className={'heroContent'}>
             <div className={'heroButtons'}>
-              
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" className={classes.mainButton}>
-                    Заказать
-                  </Button>
-                </Grid> 
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button>
-                </Grid>
-                
-              </Grid>
-              
+              <Button variant="contained" className={'mainButton'}>
+                Заказать
+              </Button>
             </div>
-          </Container>*/}
-        </div>
-        <Container className={classes.cardGrid} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="http://localhost:3000/assets/images/tort01.jpeg"
-                    title="Торт Вкусняшкин"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Вкусняшкин
-                    </Typography>
-                    <Typography>
-                      Очень вкусный торт с непередаваемыми оттенками корицы.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      Подробнее
-                    </Button>
-                    <Button size="small" color="primary">
-                      Заказать
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
-      {/* Footer */}
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="textSecondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </footer>
-      {/* End footer */}
-    </React.Fragment>
-  );
+          </div>
+          <Container className={'cardGrid'} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {this.buildTemplate()}
+            </Grid>
+          </Container>
+        </main>
+        {/* Footer */}
+        <footer className={'footer'}>
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="textSecondary"
+            component="p"
+          >
+            Something here to give the footer a purpose!
+          </Typography>
+          <Copyright />
+        </footer>
+        {/* End footer */}
+      </React.Fragment>
+    );
+  }
 }
+
+Album.propTypes = {
+  list: PropTypes.array.isRequired
+};
+
+export { Album };
